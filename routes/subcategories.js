6ef5041208d133c11e98425928931da43ddb7709
yasirraza95 <44405259+yasirraza95@users.noreply.github.com>
2022-06-23 16:43:34 +0500

    subcategory route updated

--- routes/subcategories.js
+++ routes/subcategories.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { body } = require("express-validator");
+const { check } = require("express-validator");
 const subcatController = require("../controllers/subcategories");
 const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/all", isAuth, subcatController.getAllSubcat);
-router.post("/add", isAuth, subcatController.addSubcat);
+router.post("/add", isAuth, [
+    check("name", "Name is required").notEmpty(),
+    check("cat_id", "Category Id is required").notEmpty()
+], subcatController.addSubcat);
 router.get("/get/:id", isAuth, subcatController.getSubcat);
 router.put("/update/:id", isAuth, subcatController.updateSubcat);
 router.delete("/delete/:id", isAuth, subcatController.deleteSubcat);
