80f2b006f67fc3e8a16ece02b19e1cb77270a39e
yasirraza95 <44405259+yasirraza95@users.noreply.github.com>
2022-06-23 16:46:14 +0500

    product route updated

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { body } = require("express-validator");
+const { check } = require("express-validator");
 const productController = require("../controllers/products");
 const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/all", isAuth, productController.getAllProducts);
-router.post("/add", isAuth, productController.addProduct);
+router.post("/add", isAuth, [
+    check("name", "Name is required").notEmpty(),
+    check("image", "Image is required").notEmpty(),
+    check("price", "Price is required").notEmpty(),
+    check("sub_cat_id", "Subcategory Id is required").notEmpty()
+], productController.addProduct);
 router.get("/get/:id", isAuth, productController.getProduct);
 router.put("/update/:id", isAuth, productController.updateProduct);
 router.delete("/delete/:id", isAuth, productController.deleteProduct);
