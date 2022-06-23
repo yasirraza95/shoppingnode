1be95cb1d72f54cf37a06d00ef5d269689063a52
yasirraza95 <44405259+yasirraza95@users.noreply.github.com>
2022-06-23 16:53:54 +0500

    order route updated

--- routes/orders.js
+++ routes/orders.js
@@ -5,7 +5,12 @@ const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/get/:id", isAuth, orderController.getByUserId);
-router.post("/add", isAuth, orderController.addOrder);
+router.post("/add", isAuth, [
+    check("user_id", "User Id is required").notEmpty(),
+    check("address", "Address is required").notEmpty(),
+    check("email", "Email is required").notEmpty(),
+    check("phone", "Phone is required").notEmpty()
+], orderController.addOrder);
 router.delete("/delete/:id", isAuth, orderController.deleteById);
 
 module.exports = router;
