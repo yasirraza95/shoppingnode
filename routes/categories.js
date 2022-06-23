48e99540cd00789a48c9acc039e5f7b072bafb57
yasirraza95 <44405259+yasirraza95@users.noreply.github.com>
2022-06-23 16:37:37 +0500

    categories route updated

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { body } = require("express-validator");
+const { check } = require("express-validator");
 const categoryController = require("../controllers/categories");
 const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/all", isAuth, categoryController.getAllCategories);
-router.post("/add", isAuth, categoryController.addCategory);
+router.post("/add", isAuth, [
+    check("name", "Name is required").notEmpty()
+], categoryController.addCategory);
 router.get("/get/:id", isAuth, categoryController.getCategory);
 router.put("/update/:id", isAuth, categoryController.updateCategory);
 router.delete("/delete/:id", isAuth, categoryController.deleteCategory);
