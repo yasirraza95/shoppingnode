ea8d1250e3594dea3734682eb3d702b881364e4d
yasirraza95 <44405259+yasirraza95@users.noreply.github.com>
2022-06-23 16:21:48 +0500

    cart route updated

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const { check } = require('express-validator');
 const { body } = require("express-validator");
 const cartController = require("../controllers/cart");
 const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/get/:id", isAuth, cartController.getByUserId);
-router.post("/add", isAuth, cartController.addCart);
+router.post("/add", isAuth, [
+    check("user_id", "User Id is required").notEmpty(),
+    check("prod_id", "Product Id is required").notEmpty(),
+    check("quantity", "Quantity is required").notEmpty()
+], cartController.addCart);
 router.put("/addQuantity/:id", isAuth, cartController.addQuantity);
 router.put("/removeQuantity/:id", isAuth, cartController.removeQuantity);
 router.delete("/delete/:id", isAuth, cartController.deleteById);
